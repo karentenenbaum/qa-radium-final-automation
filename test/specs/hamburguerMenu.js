c5d4f6c37718486e7f162cbf6b9861c087879bd4
karentenenbaum <kjtenenbaum@gmail.com>
2021-05-30 18:49:41 -0300

    add Hamburguer Menu tests

--- test/specs/hamburguerMenu.js
+++ test/specs/hamburguerMenu.js
@@ -16,29 +16,21 @@ describe('Hamburguer Menu Tests', () => {
     //     await browser.pause(5000);
     // });
 
-    // it("The hamburger icon should open the hamburger menu", async () => {
-    //     await (await loginPage.menuButton).click();
-    //     await browser.pause(3000);
-    //     console.log(await HamburgerMenu.hamburgerMenuWrapper);
-    //     await browser.pause(1000);
-    // });
+    it("The hamburger icon should open the hamburger menu", async () => {
+        await (await loginPage.menuButton).click();
+        await browser.pause(3000);
+        console.log(await HamburgerMenu.hamburgerMenuWrapper);
+        await browser.pause(1000);
+    });
 
-    // 	it("The hamburger menu button should close the hamburger menu", async() => {
-    // 	await (await HamburgerMenu.hamburgerCloseButton).click();
-    // 	await expect( await HamburgerMenu.hamburgerMenuWrapper).toHaveAttr("hidden")
-    //  await browser.pause(1000);
-    // });
 
-    // it('"All items" link should go to the inventory', async () => {
-    //     await (await loginPage.menuButton).click();
-    //     await (await HamburgerMenu.allItemsButton).click();
-    //     await expect(await browser.getUrl()).toBe('https://www.saucedemo.com/inventory.html')
-    //     await browser.pause(1000);
-    // });
+    it('"All items" link should go to the inventory', async () => {
+        const allItemsButton = await HamburgerMenu.allItemsButton
+        await expect(await allItemsButton.getAttribute('href')).toEqual('https://www.saucedemo.com/inventory.html#')
+        await browser.pause(1000);
+    });
 
     it("'About' link should take you to the developer's site", async () => {
-        await (await loginPage.menuButton).click();
-        await browser.pause(2000);
         const aboutButton = await HamburgerMenu.aboutButton
         await expect(await aboutButton.getAttribute('href')).toEqual('https://saucelabs.com/')
         await browser.pause(1000);
@@ -46,18 +38,6 @@ describe('Hamburguer Menu Tests', () => {
 })
 
 
-// it("'About' link should take you to the developer's site", () => {
-//     await expect(HamburgerMenu.aboutButton).toHaveHref("https://saucelabs.com/")
-//     await browser.pause(1000);
-// });
-
-// 	it('"Reset app state" button should remove everything from the cart', () => {
-// 		await (await inventory.addItem(0));
-// 		await (await inventory.addItem(1));
-// 		await (await HamburgerMenu.resetAppButton).click();
-// 		await expect(await inventory.cartButton).not.toHaveChildren();
-//         browser.pause(1000);
-// 	});
 
 // 	it('"Logout" link should go to the inventory', () => {
 // 		await (await HamburgerMenu.logoutButton).click();
--- test/specs/login.js
+++ test/specs/login.js
@@ -1,54 +1,54 @@
 const loginPage = require('../pageobjects/login.page');
 
-// describe('Login test', () => {
-//     beforeEach('Open browser', async () => {
-//         loginPage.open();
-//     })
+describe('Login test', () => {
+    beforeEach('Open browser', async () => {
+        loginPage.open();
+    })
 
-//     it('should login with valid credentials', async () => {
-//         await loginPage.login(loginPage.correctUsername, loginPage.correctPassword);
-//         await expect(await browser.getUrl()).toBe("https://www.saucedemo.com/inventory.html");
-//         await (await loginPage.menuButton).click();
-//         await browser.pause(1000);
-//         await (await loginPage.logoutButton).click();
-//         await browser.pause(2000);
-//     });
-
-
-//     it('should not login with invalid credentials: Wrong username and wrong password', async () => {
-//         await loginPage.login('user_invalid', 'secret_couce');
-//         const message = await (await loginPage.loginErrorMessage).getText();
-//         await expect(message).toMatch('Epic sadface: Username and password do not match any user in this service');
-//     });
-
-//     it('should not login with invalid credentials: Right username and wrong password', async () => {
-// 		await loginPage.login('standard_user', 'invalid_password');
-// 	    await expect(loginPage.loginErrorMessage);
-//         const message = await (await loginPage.loginErrorMessage).getText();
-//         await expect(message).toMatch('Epic sadface: Username and password do not match any user in this service');
-//     });
-
-//     it('should not login with invalid credentials: Wrong username and right password', async () => {
-//         await loginPage.login('invalid_user', 'secret_sauce');
-//         const message = await (await loginPage.loginErrorMessage).getText();
-//         await expect(message).toMatch('Epic sadface: Username and password do not match any user in this service');
-//         await browser.pause(1000);
-//     });
+    it('should login with valid credentials', async () => {
+        await loginPage.login(loginPage.correctUsername, loginPage.correctPassword);
+        await expect(await browser.getUrl()).toBe("https://www.saucedemo.com/inventory.html");
+        await (await loginPage.menuButton).click();
+        await browser.pause(1000);
+        await (await loginPage.logoutButton).click();
+        await browser.pause(2000);
+    });
 
 
-//     it('should not login with invalid credentials: Empty username and right password', async () => {
-//         await loginPage.login('', 'secret_sauce');
-//         const message = await (await loginPage.loginErrorMessage).getText();
-//         await browser.pause(1000);
-//         await expect(message).toMatch('Epic sadface: Username is required');
-//     });
+    it('should not login with invalid credentials: Wrong username and wrong password', async () => {
+        await loginPage.login('user_invalid', 'secret_couce');
+        const message = await (await loginPage.loginErrorMessage).getText();
+        await expect(message).toMatch('Epic sadface: Username and password do not match any user in this service');
+    });
+
+    it('should not login with invalid credentials: Right username and wrong password', async () => {
+		await loginPage.login('standard_user', 'invalid_password');
+	    await expect(loginPage.loginErrorMessage);
+        const message = await (await loginPage.loginErrorMessage).getText();
+        await expect(message).toMatch('Epic sadface: Username and password do not match any user in this service');
+    });
+
+    it('should not login with invalid credentials: Wrong username and right password', async () => {
+        await loginPage.login('invalid_user', 'secret_sauce');
+        const message = await (await loginPage.loginErrorMessage).getText();
+        await expect(message).toMatch('Epic sadface: Username and password do not match any user in this service');
+        await browser.pause(1000);
+    });
 
 
-//     it('should not login with invalid credentials: Right username and empty password', async () => {
-//         await loginPage.login('standard_user', '');
-//         const message = await (await loginPage.loginErrorMessage).getText();
-//         await browser.pause(1000);
-//         await expect(message).toMatch('Epic sadface: Password is required');
-// 	});
+    it('should not login with invalid credentials: Empty username and right password', async () => {
+        await loginPage.login('', 'secret_sauce');
+        const message = await (await loginPage.loginErrorMessage).getText();
+        await browser.pause(1000);
+        await expect(message).toMatch('Epic sadface: Username is required');
+    });
 
-// });
+
+    it('should not login with invalid credentials: Right username and empty password', async () => {
+        await loginPage.login('standard_user', '');
+        const message = await (await loginPage.loginErrorMessage).getText();
+        await browser.pause(1000);
+        await expect(message).toMatch('Epic sadface: Password is required');
+	});
+
+});
